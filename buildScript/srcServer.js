24c4041dd091e08c8f2f19be2ee48e1a36e18b58
subinviswanathan@gmail.com <subinviswanathan@gmail.com>
2018-08-22 23:22:32 +0530

    added webpack for bundling

--- buildScript/srcServer.js
+++ buildScript/srcServer.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var path = require('path');
+var webpack = require('webpack');
+var config = require('../webpack.config.dev');
 
 var port = 3000;
 var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+noInfo: true,
+publicPath: config.output.publicPath
+}));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
