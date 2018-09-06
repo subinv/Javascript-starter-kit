2cf77e7242259cd7159f68dcf04e71a096281485
subinviswanathan@gmail.com <subinviswanathan@gmail.com>
2018-09-06 23:06:22 +0530

    delete functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import numeral from 'numeral';
-import { getUsers } from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 const value = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${value} for this`); // eslint-disable-line no-console
@@ -17,4 +17,17 @@ getUsers().then(result => {
     </tr>`
   });
   global.document.querySelector('#users').innerHTML = userBody;
+
+  const deleteLinks = global.document.querySelectorAll('.deleteUser');
+  //Must use array.from to create a real array from a dom collection
+  //querySelectorAll returns an array like object
+  Array.from(deleteLinks, link => {
+    link.onclick = (event) => {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes['data-id'].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
 });
